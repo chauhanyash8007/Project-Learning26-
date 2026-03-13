cedb05a5c77ccedcfab288f89e3223516243397e
Chauhan Yash <146646531+chauhanyash8007@users.noreply.github.com>
2026-03-13 15:57:53 +0530

    used react-toastify

--- MERN/frontend/learning26carscout/src/components/user/GetApiDemo.jsx
+++ MERN/frontend/learning26carscout/src/components/user/GetApiDemo.jsx
@@ -1,43 +1,69 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 export const GetApiDemo = () => {
-    const [users, setusers] = useState([])
+  const [users, setusers] = useState([]);
 
-    const getUsers = async()=>{
+  const getUsers = async () => {
+    const res = await axios.get("https://node5.onrender.com/user/user/");
+    console.log("response...", res);
+    setusers(res.data.data);
+  };
 
-        const res = await axios.get("https://node5.onrender.com/user/user/")
-        console.log("response...",res);
-        setusers(res.data.data)
+  const deleteUser = async (id) => {
+    //alert("delete user called..."+id)
+    ///url =de5.onrender.com/user/user/12345678o9p
+
+    const res = await axios.delete(
+      `https://node5.onrender.com/user/user/${id}`,
+    );
+    console.log(res);
+    if (res.status == 204) {
+      toast.success("User deleted successfully");
+      getUsers();
     }
-    //component --> load --> useEffec call --> function call..
-    useEffect(()=>{
-        //api logic..
-        // eslint-disable-next-line react-hooks/set-state-in-effect
-        getUsers()
-    },[])
+  };
+  //component --> load --> useEffec call --> function call..
+  useEffect(() => {
+    //api logic..
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    getUsers();
+  }, []);
   return (
-    <div style={{textAlign:"center"}}>
-        <h1>GET API DEMO</h1>
-        {/* <button onClick={getUsers}>Get Users</button> */}
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                </tr>
-            </thead>
-            <tbody>
-                {users.map((user)=>(
-                    <tr key={user._id}>
-                        <td>{user._id}</td>
-                        <td>{user.name}</td>
-                        <td>{user.email}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+    <div style={{ textAlign: "center" }}>
+      <h1>GET API DEMO</h1>
+      {/* <button onClick={getUsers}>Get Users</button> */}
+      {/* tailwind classes for table */}
+      <table className="table table-bordered table-hover table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user._id}>
+              <td>{user._id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>
+                <button
+                  onClick={() => {
+                    deleteUser(user._id);
+                  }}
+                  className="text-red-500 cursor-pointer font-bold"
+                >
+                  DELETE
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
